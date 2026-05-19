36962a1d1d94600e192959cc095c12d35c1fdc4f
ZhengLin <lzzg1994@outlook.com>
2026-05-19 14:54:39 +1000

    Initial commit

---
+++ schemaTypes/product.js
@@ -0,0 +1,75 @@
+export default {
+  name: 'product',
+  title: 'Product',
+  type: 'document',
+  fields: [
+    { name: 'name', type: 'string' },
+    { name: 'brand', title: 'Brand', type: 'string',
+        options: {
+            list: ['Ball','Burberry','Bulova','Bvlgari','Cartier','Casio','Citizen',
+            'Corum','Cyma','Ebel','Elgin','Eterna','Fendi','Gucci','Hamilton',
+            'Hermès','Heuer','IWC','Jaeger-LeCoultre','Junghans','KUOE','Longines','Omega',
+            'Piaget','Rado','Revue Thommen','Rolex','Seiko','Sinn','Tissot','Tudor',
+            'Ulysse Nardin','Van Cleef','Waltham','Other',
+            ],
+        },
+    },
+
+    {
+      name: 'slug',
+      type: 'slug',
+      options: { source: 'name' },
+    },
+
+    // ⭐ 多图（重点）
+    {
+      name: 'images',
+      title: 'Product Images',
+      type: 'array',
+      of: [{ type: 'image' }],
+      validation: (Rule) => Rule.max(12),
+    },
+
+    {
+      name: 'price',
+      type: 'number',
+    },
+
+    {
+        name: 'gender',
+        title: 'Gender',
+        type: 'array',
+        of: [
+            {
+            type: 'string',
+            options: {
+                list: ["Men's", "Women's", 'Unisex'],
+            },
+            },
+        ],
+    },
+
+    {
+      name: 'movement',
+      title: 'Movement Type',
+      type: 'string',
+      options: {
+        list: ['Automatic', 'Manual Wind', 'Quartz', 'Tuning Fork', 'Pocket Watch'],
+      },
+    },
+
+    {
+      name: 'status',
+      type: 'string',
+      options: {
+        list: ['available', 'sold'],
+      },
+      initialValue: 'available',
+    },
+
+    {
+      name: 'description',
+      type: 'text',
+    },
+  ],
+}
